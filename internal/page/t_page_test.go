544048800464b2519f251dd6d47ee2a4acf56caa
Tudor Hulban <prindex2010@gmail.com>
2021-02-22 17:22:14 +0200

    with elements

--- internal/page/t_page_test.go
+++ internal/page/t_page_test.go
@@ -11,8 +11,18 @@ func TestPage(t *testing.T) {
 	p, _ := NewPage(nil)
 
 	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
-		Name: "1",
-		HTML: "x",
+		Name: "Page Start",
+		HTML: PageStart,
+	}))
+
+	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
+		Name: "Head",
+		HTML: HEAD,
+	}))
+
+	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
+		Name: "Body Start",
+		HTML: BODYStart,
 	}))
 
 	level := p.GetCurrentPos() + 1
@@ -21,17 +31,27 @@ func TestPage(t *testing.T) {
 		HTML: "y",
 	}))
 
-	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
-		Name: "3",
-		HTML: "xxxxxxxx",
-	}))
-
 	require.Nil(t, p.Add(level, &Node{
 		Name: "2b",
 		HTML: "z",
 	}))
 
-	require.Equal(t, len(p.Nodes), 3)
+	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
+		Name: "Body End",
+		HTML: BODYEnd,
+	}))
+
+	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
+		Name: "Footer",
+		HTML: FOOTER,
+	}))
+
+	require.Nil(t, p.Add(p.GetCurrentPos()+1, &Node{
+		Name: "Page End",
+		HTML: PageEnd,
+	}))
+
+	//require.Equal(t, len(p.Nodes), 4)
 
 	fmt.Println("Page:")
 	fmt.Println(p.GetString())
